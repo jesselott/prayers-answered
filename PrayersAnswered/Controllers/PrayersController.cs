3af2e92c68ed8df69d111759805dc9e4e29aee06
jesse <jesse@MYCOMPY>
2017-02-01 07:56:11 -0500

    Added view model.

---
+++ PrayersAnswered/Controllers/PrayersController.cs
@@ -0,0 +1,20 @@
+﻿using PrayersAnswered.Models;
+using System.Web.Mvc;
+
+namespace PrayersAnswered.Controllers
+{
+    public class PrayersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PrayersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrayersAnswered/ViewModel/PrayerFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrayersAnswered.ViewModel
+{
+    public class PrayerFormViewModel
+    {
+        public string Title { get; set; }
+        public string  Content { get; set; }
+    }
+}
